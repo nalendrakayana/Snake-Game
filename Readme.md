d14d5146c406613e78d24d76e3d4b29c689c43fb
nalendrakayana <nalendrakayana12@gmail.com>
2022-06-11 14:32:12 +0700

    Added new file data to keep track high score player

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 # Snake Game Udemy Project
 
-So this is my first OOP project using Python i learned so much things about OOP, how to use module, and even how to inherit from another class using turtle module
+So this is my first OOP project using Python i learned so much things about OOP, how to use module,how to inherit from another class using turtle module, and even how to read file or write file using python.
+
--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", mode="r") as high_score:
+            self.high_score = int(high_score.read())
         self.penup()
         self.hideturtle()
         self.color("white")
@@ -18,6 +19,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as high_score:
+                high_score.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
 
